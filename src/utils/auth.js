97f3c2c63931e132e5abeda56f3cd84cd2bc9625
Bogdan <bpintilie16@gmail.com>
2023-07-31 19:10:18 +0300

    Update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -14,6 +14,7 @@ export const signup = (user) => {
 };
 
 export const signin = (email, password) => {
+  console.log(baseUrl);
   return fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers: {
